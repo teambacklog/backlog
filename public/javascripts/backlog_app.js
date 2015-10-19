f68e850d1b5c0c373769cb7c5ecaa61a5416ce2d
Bo Yuan <yuanbo@outlook.com>
2015-10-19 15:11:06 -0400

    show and hide of the task list

--- public/javascripts/backlog_app.js
+++ public/javascripts/backlog_app.js
@@ -6,5 +6,16 @@ $(document).ready(function(){
     $('#contextMenu').toggle('slow');
   });
 
+  $('#switchBox').click(function() {
+    var $this = $(this);
+    // $this will contain a reference to the checkbox
+    if ($this.is(':checked')) {
+        // the checkbox was checked
+        $('#taskList').show('slow');
 
+    } else {
+        // the checkbox was unchecked
+        $('#taskList').hide('slow');
+    }
+});
 });
