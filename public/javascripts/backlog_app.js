bc5fadfd820ffc37f096c0cfea0c9eccf1da71e1
Bo Yuan <yuanbo@outlook.com>
2015-10-19 15:51:43 -0400

    create a good looking context menu(add new task) half done

--- public/javascripts/backlog_app.js
+++ public/javascripts/backlog_app.js
@@ -17,5 +17,14 @@ $(document).ready(function(){
         // the checkbox was unchecked
         $('#taskList').hide('slow');
     }
-});
+  });
+
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
+  $('input#input_text, textarea#textarea1').characterCounter();
+
+  $('select').material_select();
 });
