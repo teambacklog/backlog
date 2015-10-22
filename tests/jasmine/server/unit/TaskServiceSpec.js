afc067cfe112a18b2ff14be692b9dc09e430d95d
Yi Lei <lueaon@gmail.com>
2015-10-22 16:07:11 -0400

    Added Velocity and Jasmine and related directories for unit testing.

---
+++ tests/jasmine/server/unit/TaskServiceSpec.js
@@ -0,0 +1,17 @@
+describe('TaskService', function() {
+  'use strict';
+
+  beforeEach(function () {
+    MeteorStubs.install();
+    // Need to sign up and sign up w/ a temporary user
+  });
+
+  afterEach(function () {
+    MeteorStubs.uninstall();
+  });
+
+  describe('addTask', function () {
+    it('should add a task to the user')
+  });
+
+});
