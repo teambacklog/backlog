f05c8369afcbd43835bfd72d3db208c18aa8a2ca
Yi Lei <lueaon@gmail.com>
2015-10-29 16:14:08 -0400

    Forgot to include the update backlog_client.js

--- client/backlog_client.js
+++ client/backlog_client.js
@@ -27,13 +27,60 @@ Template.body.helpers({
   },
 });
 
+Template.userSummary.onRendered( function renderFrontPage() {
+  $('#contextMenu').hide();
+  $('#taskList').hide();
+  $(".modal-trigger").leanModal();
+});
+
+
+Template.userSummary.events({
+  'change #switchBox': function switchTaskDisplay(event) {
+    const box = event.target;
+    // $this will contain a reference to the checkbox
+    if ( box.checked ) {
+      // the checkbox was checked
+      $('#taskList').show('slow');
+      $('#taskSummaryBoard').hide('slow');
+    } else {
+      // the checkbox was unchecked
+      $('#taskList').hide('slow');
+      $('#taskSummaryBoard').show('slow');
+    }
+  },
+  'click #add-task-button': function showContextMenu() {
+    $('#contextMenu').toggle('slow');
+  },
+});
+
+Template.taskList.onRendered( function taskListOnDisplay() {
+  $('.scroll').slimScroll({
+    height: '250px',
+  });
+});
+
+Template.addTaskDisplay.onRendered( function renderContextMenu() {
+  // $(".modal-content").leanModal();
+  $('#datepicker').datepicker();
+  $( '#slider').slider({
+    range: 'min',
+    value: 15,
+    min: 15,
+    max: 300,
+    step: 15,
+    slide: function(event, ui) {
+      $('#task_est_time').val( ui.value );
+    },
+  });
+
+  // $('select').material_select();
+});
+
 // allows addTask.html to add tasks
 Template.addTaskDisplay.events({
   // submit information for new row
   'click #submit-new-task': function addTask(event, template) {
     event.preventDefault();
-    // unique Meteor variable
-    // text from text fields
     const name = template.find('[name="name"]').value;
 
     const priorityField = template.find('[name="priority"]');
@@ -41,9 +88,18 @@ Template.addTaskDisplay.events({
 
     const date = template.find('[name="date"]').value;
     const time = template.find('[name="est"]').value;
-    Meteor.call('addTask', name, priority, date, time);
+
+    Meteor.call('addTask', name, priority, date, time, null);
     // Session.set('showAddTask', false);
     // Session.set('showFrontPage', true);
+  },
+  'click #remove-new-task': function shrinkContextMenu(event) {
+    event.preventDefault();
+    $('#contextMenu').hide('slow');
+  },
+  'change #slider': function moveSlider(event) {
+    event.preventDefault();
+    $('#amount').val('$' + $('#slider').slider('value'));
   },
 });
 
