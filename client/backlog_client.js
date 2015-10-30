d3ede7ae17f7e710322e8e141500f4a599fa1005
Yi Lei <lueaon@gmail.com>
2015-10-30 19:15:04 -0400

    Renamed taskSummaryBoard to taskSummary and userSummary to userBoard.
    Also,
    1. Added .eslintignore to ignore external lib in public.
    2. Linted some of the code.
    3. Ordered the functions in backlog_client.js in alphabetical order.

--- client/backlog_client.js
+++ client/backlog_client.js
@@ -20,46 +20,35 @@ Meteor.startup( function start() {
 //   return Session.get('resize');
 // };
 
-// defines what 'tasks' and 'contextMenus' are when referenced in the HTML doc
-Template.body.helpers({
-  tasks: function getTasks() {
-    return Tasks.find();
+// allows addTask.html to add tasks
+Template.addTaskDisplay.events({
+  // submit information for new row
+  'click #submit-new-task': function addTask(event, template) {
+    event.preventDefault();
+    const name = template.find('[name="name"]').value;
+
+    const priorityField = template.find('[name="priority"]');
+    const priority = priorityField.options[priorityField.selectedIndex].text;
+
+    const date = template.find('[name="date"]').value;
+    const time = template.find('[name="est"]').value;
+
+    Meteor.call('addTask', name, priority, date, time, null);
+    $('#addTaskModal').hide('slow');
+    // Session.set('showAddTask', false);
+    // Session.set('showFrontPage', true);
+  },
+  'click #remove-new-task': function shrinkContextMenu(event) {
+    event.preventDefault();
+    $('#addTaskModal').hide('slow');
+  },
+  'change #slider': function moveSlider(event) {
+    event.preventDefault();
+    $('#amount').val('$' + $('#slider').slider('value'));
   },
 });
 
-Template.userSummary.onRendered( function renderFrontPage() {
-  $('#contextMenu').hide();
-  $('#taskList').hide();
-  $(".modal-trigger").leanModal();
-});
-
-
-Template.userSummary.events({
-  'change #switchBox': function switchTaskDisplay(event) {
-    const box = event.target;
-    // $this will contain a reference to the checkbox
-    if ( box.checked ) {
-      // the checkbox was checked
-      $('#taskList').show('slow');
-      $('#taskSummaryBoard').hide('slow');
-    } else {
-      // the checkbox was unchecked
-      $('#taskList').hide('slow');
-      $('#taskSummaryBoard').show('slow');
-    }
-  },
-  'click #add-task-button': function showContextMenu() {
-    $('#contextMenu').toggle('slow');
-  },
-});
-
-Template.taskList.onRendered( function taskListOnDisplay() {
-  $('.scroll').slimScroll({
-    height: '250px',
-  });
-});
-
-Template.addTaskDisplay.onRendered( function renderContextMenu() {
+Template.addTaskDisplay.onRendered(function renderContextMenu() {
   // $(".modal-content").leanModal();
   $('#datepicker').datepicker();
   $( '#slider').slider({
@@ -76,33 +65,6 @@ Template.addTaskDisplay.onRendered( function renderContextMenu() {
   // $('select').material_select();
 });
 
-// allows addTask.html to add tasks
-Template.addTaskDisplay.events({
-  // submit information for new row
-  'click #submit-new-task': function addTask(event, template) {
-    event.preventDefault();
-    const name = template.find('[name="name"]').value;
-
-    const priorityField = template.find('[name="priority"]');
-    const priority = priorityField.options[priorityField.selectedIndex].text;
-
-    const date = template.find('[name="date"]').value;
-    const time = template.find('[name="est"]').value;
-
-    Meteor.call('addTask', name, priority, date, time, null);
-    // Session.set('showAddTask', false);
-    // Session.set('showFrontPage', true);
-  },
-  'click #remove-new-task': function shrinkContextMenu(event) {
-    event.preventDefault();
-    $('#contextMenu').hide('slow');
-  },
-  'change #slider': function moveSlider(event) {
-    event.preventDefault();
-    $('#amount').val('$' + $('#slider').slider('value'));
-  },
-});
-
 // Find tasks specific to this user
 Template.taskList.helpers({
   tasks: function getTasks() {
@@ -110,20 +72,50 @@ Template.taskList.helpers({
   },
 });
 
+Template.taskList.onRendered( function taskListOnDisplay() {
+  $('.scroll').slimScroll({
+    height: '250px',
+  });
+});
+
 // Find tasks specific to this user
-Template.taskSummaryBoard.helpers({
+Template.taskSummary.helpers({
   taskCount: function getTaskCounts() {
     return Tasks.find().count();
   },
 });
 
-Template.userSummary.events({
+Template.userBoard.events({
+  /*
+  'change #switchBox': function switchTaskDisplay(event) {
+    const box = event.target;
+    // $this will contain a reference to the checkbox
+    if ( box.checked ) {
+      // the checkbox was checked
+      $('#taskList').show('slow');
+      $('#taskSummary').hide('slow');
+    } else {
+      // the checkbox was unchecked
+      $('#taskList').hide('slow');
+      $('#taskSummary').show('slow');
+    }
+  },*/
   'change #switchBox': function toggleSummary(event) {
     event.preventDefault();
     Session.set('displayTaskSummary', !Session.get('displayTaskSummary'));
   },
+  'click #add-task-button': function showContextMenu() {
+    $('#addTaskModal').show('slow');
+  },
 });
 
+Template.userBoard.onRendered(function renderFrontPage() {
+  $('[name="addTaskDisplay"]').hide();
+  $('#taskList').hide();
+  $('.modal-trigger').leanModal();
+});
+
+// Global function
 Template.registerHelper('displayTaskSummary', function() {
   return Session.get('displayTaskSummary');
 });
