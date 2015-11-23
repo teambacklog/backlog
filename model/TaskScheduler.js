1cd237b4abe51a7de3faf9027ddfa178e0dc4ad9
Yi Lei <lueaon@gmail.com>
2015-11-22 23:37:44 -0500

    Some refactoring and lint.

--- model/TaskScheduler.js
+++ model/TaskScheduler.js
@@ -6,12 +6,17 @@ TaskScheduler = {
     return this._earliestDueDate();
   },
   _earliestDueDate: function TaskScheduler$earliestDueDate() {
-    let allTasks = Tasks.find({}, { sort: { 'task._deadline': 1 } }).fetch();
+    var earliestTask;
+    var allTasks;
+    var i;
+
+    allTasks = Tasks.find({}, { sort: { 'task._deadline': 1 } }).fetch();
     if (allTasks.length === 0) {
       return 0;
     }
-    var earliestTask = null;
-    for (var i = 0; i < allTasks.length; i += 1) {
+
+    earliestTask = null;
+    for (i = 0; i < allTasks.length; i += 1) {
       if (allTasks[i].timeRemaining !== 0) {
         earliestTask = allTasks[i];
         break;
@@ -20,7 +25,6 @@ TaskScheduler = {
     if (earliestTask === null) {
       return 0;
     }
-    console.log(earliestTask.timeRemaining);
     return earliestTask;
   },
 };
